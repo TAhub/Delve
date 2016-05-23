502e74669c5351b895e7a635db33cda25fd99b16
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-05-22 23:11:11 -0700

    Wrote some of the code for the how to play controller. Now it just needs, you know, text and pictures.

--- Delve/HowToPlayViewController.m
+++ Delve/HowToPlayViewController.m
@@ -17,45 +17,84 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *returnButton;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UIButton *previousButton;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
 
-
-
+@property int pageOn;
 
 @end
 
 @implementation HowToPlayViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 	
 	[self formatButton:self.returnButton];
+	[self formatButton:self.previousButton];
+	[self formatButton:self.nextButton];
 	[self formatPanel:self.titlePanel];
 	[self formatPanel:self.textPanel];
 	[self formatPanel:self.returnPanel];
 	self.titleLabel.textColor = loadColorFromName(@"ui text");
+	self.textLabel.textColor = loadColorFromName(@"ui text");
 	
-	//TODO: things to explain in this:
-	//	what all the things on the upper UI bar mean
-	//	how the combat works (dodge, block, and unlock as a resource)
-	//	some basic description of elements, etc
-	//	a mention of warning indicators, and what they mean
-	//	a simple description of your objective (get to the final floor, where a functional Eol vessel awaits)
+	self.pageOn = 0;
+	
+	[self formatPage];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)formatPage
+{
+	//TODO: change the contents of the text label to contain the correct text, and the image to contain the right image
+	
+	//TODO: screen structure
+	//all screenshots should be about as wide as the screen, but significantly narrower vertically
+	//PAGE 0:
+	//	a picture of the top bar (make a character with finesse, shield, and dodge, to explain the concept of those icons)
+	//	text is an explanation of each row
+	//PAGE 1:
+	//	a screenshot of that sample character in a fight on the first floor
+	//	text is a basic overview of combat
+	//		dodges and blocks are resources that refill when not in a fight, or manually with dodge and other special moves
+	//		some attacks cannot be blocked or dodged, as mentioned in their descriptions
+	//		many moves have minimum or maximum range
+	//		every attack has an element; some enemies may be resistant or weak to various elements
+	//PAGE 2:
+	//	a screenshot of a guardian on top of a teleport pad
+	//	text is a simple statement of your goal and very barebones plot overview
+	//		this series of caverns inhabited by the ancient Eol was recently re-discovered
+	//		everyone wants to get to the bottom-most layer, where there is a functional Eol vessel (maybe super-quick faction overview if there's room)
+	//		you were, for some reason or another, exiled; your only hope is to get past EVERYONE to get to the vessel
+	//		get to the teleporter at the top of each floor to continue down
+	
+	
+	//hide buttons to keep you from going off of the text
+	self.previousButton.hidden = self.pageOn == 0;
+	self.nextButton.hidden = self.pageOn == 2;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)previousPress
+{
+	if (self.pageOn > 0)
+	{
+		self.pageOn -= 1;
+		[self formatPage];
+	}
 }
-*/
+
+- (IBAction)nextPress
+{
+	if (self.pageOn < 2)
+	{
+		self.pageOn += 1;
+		[self formatPage];
+	}
+}
+
+
 
 @end
